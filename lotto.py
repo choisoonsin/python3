50531b3f0026c9bd83cb914aab01a62fd1c11f0b
choiyounghoon <cyh0214@gmail.com>
2020-02-23 10:28:25 +0900

    pyqt and scraping example

--- lotto.py
+++ lotto.py
@@ -1,3 +1,35 @@
 import random
 
-print(random.randrange(0 , 10))
+def get_lotto(manual_lotto=[]):
+    if len(manual_lotto) == 6:
+        return manual_lotto
+    
+    lotto = []
+    while(len(lotto) <= 6):
+        lotto.append(random.randrange(1 , 45))
+        lotto = list(set(lotto))
+
+    # lotto.sort(reverse=True)
+    lotto.sort()
+    return lotto
+
+print("auto:", get_lotto())
+
+print("manual", get_lotto([1,3,5,7,9,10]))
+ 
+while(True):
+    prom = input("Please insert key\nexit or lotto count you want\n:")
+    if prom == "exit":
+        break
+    
+    if prom.isdigit():
+        x = 1
+        while(x <= int(prom)):
+            x += 1
+            print(get_lotto())
+
+ 
+    
+
+
+
